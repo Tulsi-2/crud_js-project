e29b1e47e15005373413714fdb61702b5d7dc0a2
Tulsi-2 <MY_NAME@example.com>
2023-09-11 23:07:31 +0530

    commit

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
 const btn = document.querySelector('button')
 
+
 btn.addEventListener('click', () => {
-    const name = document.querySelector('#name').value
+    const name= document.querySelector('#name').value
 
     const email = document.querySelector('#email').value
 
     const phone = document.querySelector('#phone').value
 
-    let data = {
-        user_name  : name,
-        user_email : email,
-        user_phone  : phone
-
+    
+    if((name == "") && (email == "") && (phone == "")){
+        alert("Please fill all the fields")
+    
     }
-    console.log(data)
-
-    let arr = []
-    arr.push(data)
-    console.log(arr)
-
-    arr.map((item)=>{
-        const tableData = document.querySelector('tbody')
-        console.log(item)
-        let row = document.createElement('tr')
-        let rowData1 = document.createElement('td')
-        let rowData2 = document.createElement('td')
-        let rowData3 = document.createElement('td')
-        let rowData4 = document.createElement('td')
-
-        const deleteBtn = document.createElement('button')
-
-        tableData.appendChild(row)
-        row.appendChild(rowData1)
-        row.appendChild(rowData2)
-        row.appendChild(rowData3)
-        row.appendChild(rowData4)
-        rowData4.appendChild(deleteBtn)
-        deleteBtn.innerText = "Delete"
-
-        deleteBtn.addEventListener('click',()=>{
-            row.remove()
+    else{
+        let data = {
+            user_name  : name,
+            user_email : email,
+            user_phone  : phone
+        
+        }
+        console.log(data)
+        
+        let arr = []
+        arr.push(data)
+        console.log(arr)
+        
+        arr.map((item)=>{
+            const tableData = document.querySelector('tbody')
+            console.log(item)
+            let row = document.createElement('tr')
+            let rowData1 = document.createElement('td')
+            let rowData2 = document.createElement('td')
+            let rowData3 = document.createElement('td')
+            let rowData4 = document.createElement('td')
+        
+            const deleteBtn = document.createElement('button')
+        
+            tableData.appendChild(row)
+            row.appendChild(rowData1)
+            row.appendChild(rowData2)
+            row.appendChild(rowData3)
+            row.appendChild(rowData4)
+            rowData4.appendChild(deleteBtn)
+            deleteBtn.innerText = "Delete"
+        
+            deleteBtn.addEventListener('click',()=>{
+                row.remove()
+            })
+        
+            rowData1.innerText = item.user_name
+            rowData2.innerText = item.user_email
+            rowData3.innerText = item.user_phone
+        
         })
+        
+    }
 
-        rowData1.innerText = item.user_name
-        rowData2.innerText = item.user_email
-        rowData3.innerText = item.user_phone
-
-    })
-
+    resetFunction()
     // let tableData = document.querySelector('tbody')
 
     // let newRow = document.createElement('tr')
@@ -66,6 +76,15 @@ btn.addEventListener('click', () => {
     // newData3.innerText =phone
 
 })
+ const resetFunction =() =>{
+    name.value = ""
+    email.value = ""
+    phone.value = ""
+    console.log(name)
+    console.log(email)
+    console.log(phone)
+ 
+}
 
 
 
