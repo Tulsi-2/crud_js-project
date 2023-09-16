a628675aa202f69fc80a5e38adba08721ce91223
Tulsi-2 <MY_NAME@example.com>
2023-09-16 23:18:12 +0530

    commit

--- script.js
+++ script.js
@@ -1,90 +1,86 @@
-const btn = document.querySelector('button')
+const btn = document.querySelector("button");
 
+btn.addEventListener("click", () => {
+  const name = document.querySelector("#name").value;
 
-btn.addEventListener('click', () => {
-    const name= document.querySelector('#name').value
+  const email = document.querySelector("#email").value;
 
-    const email = document.querySelector('#email').value
+  const phone = document.querySelector("#phone").value;
 
-    const phone = document.querySelector('#phone').value
+  if (name == "" || email == "" || phone == "") {
+    alert("Please fill all the fields");
+  } else {
+    let data = {
+      user_name: name,
+      user_email: email,
+      user_phone: phone,
+    };
+    console.log(data);
 
-    
-    if((name == "") || (email == "") || (phone == "")){
-        alert("Please fill all the fields")
-    
-    }
-    else{
-        let data = {
-            user_name  : name,
-            user_email : email,
-            user_phone  : phone
-        
-        }
-        console.log(data)
-        
-        let arr = []
-        arr.push(data)
-        console.log(arr)
-        
-        arr.map((item)=>{
-            const tableData = document.querySelector('tbody')
-            console.log(item)
-            let row = document.createElement('tr')
-            let rowData1 = document.createElement('td')
-            let rowData2 = document.createElement('td')
-            let rowData3 = document.createElement('td')
-            let rowData4 = document.createElement('td')
-        
-            const deleteBtn = document.createElement('button')
-        
-            tableData.appendChild(row)
-            row.appendChild(rowData1)
-            row.appendChild(rowData2)
-            row.appendChild(rowData3)
-            row.appendChild(rowData4)
-            rowData4.appendChild(deleteBtn)
-            deleteBtn.innerText = "Delete"
-        
-            deleteBtn.addEventListener('click',()=>{
-                row.remove()
-            })
-        
-            rowData1.innerText = item.user_name
-            rowData2.innerText = item.user_email
-            rowData3.innerText = item.user_phone
-        
-        })
-        
+    let arr = [];
+    arr.push(data);
+
+    let m = localStorage.getItem("ary");
+    if (m == null) {
+      const jsonData = JSON.stringify(arr);
+      localStorage.setItem("ary", jsonData);
+    } else {
+      let oldData = JSON.parse(localStorage.getItem("ary"));
+      oldData.push(data);
+      localStorage.setItem("ary", JSON.stringify(oldData));
     }
 
-    resetFunction()
-    // let tableData = document.querySelector('tbody')
+    let parseArr = JSON.parse(localStorage.getItem("ary"))
+    console.log(parseArr)
 
-    // let newRow = document.createElement('tr')
-    // let newData1 = document.createElement('td')
-    // let newData2 = document.createElement('td')
-    // let newData3 = document.createElement('td')
+    parseArr.map((item) => {
+      const tableData = document.querySelector("tbody");
+      console.log(item);
+      let row = document.createElement("tr");
+      let rowData1 = document.createElement("td");
+      let rowData2 = document.createElement("td");
+      let rowData3 = document.createElement("td");
+      let rowData4 = document.createElement("td");
 
-    // tableData.appendChild(newRow)
+      const deleteBtn = document.createElement("button");
 
-    // newRow.appendChild(newData1)
-    // newRow.appendChild(newData2)
-    // newRow.appendChild(newData3)
+      tableData.appendChild(row);
+      row.appendChild(rowData1);
+      row.appendChild(rowData2);
+      row.appendChild(rowData3);
+      row.appendChild(rowData4);
+      rowData4.appendChild(deleteBtn);
+      deleteBtn.innerText = "Delete";
 
-    // newData1.innerText = name
-    // newData2.innerText = email
-    // newData3.innerText =phone
+      deleteBtn.addEventListener("click", () => {
+        row.remove();
+      });
 
-})
- const resetFunction =() =>{
-    name.value = ""
-    email.value = ""
-    phone.value = ""
-    console.log(name)
-    console.log(email)
-    console.log(phone)
- 
-}
+      rowData1.innerText = item.user_name;
+      rowData2.innerText = item.user_email;
+      rowData3.innerText = item.user_phone;
+    });
 
+    resetFunction();
+  }
 
+  // let newRow = document.createElement('tr')
+  // let newData1 = document.createElement('td')
+  // let newData2 = document.createElement('td')
+  // let newData3 = document.createElement('td')
 
+  // tableData.appendChild(newRow)
+
+  // newRow.appendChild(newData1)
+  // newRow.appendChild(newData2)
+  // newRow.appendChild(newData3)
+
+  // newData1.innerText = name
+  // newData2.innerText = email
+  // newData3.innerText =phone
+});
+const resetFunction = () => {
+  name.value = "";
+  email.value = "";
+  phone.value = "";
+};
